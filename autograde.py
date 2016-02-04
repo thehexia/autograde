648845e4cf49716fd0c3014e6681b9b263566036
Hoang Nguyen <thehexia@gmail.com>
2016-02-04 09:04:37 -0500

    progress

--- autograde.py
+++ autograde.py
@@ -8,7 +8,13 @@ res_folder = "RESULT_" + os.path.basename(sys.argv[1]);
 # for every file in the folder
 for filename in os.listdir(folder):
     # result text is stored in the folder
-    res_path = res_folder + '/' +
+    res_path = res_folder + '/' + filename + ".txt"
+    res_path = os.path.abspath(res_path)
+
+    if not res_path:
+        print("ERROR: SAVED FAIL")
+        break
+
     # only compile the .cpp files.
     if filename.endswith('.cpp'):
         call(["g++", "-std=c++11", filename])
